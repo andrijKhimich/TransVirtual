e1fc6c65dcdeccc4d75d275e0c380ea8c5a7e954
Andrew Khimich <andrijkhimich@gmail.com>
2022-12-08 14:29:25 +0200

    blog inner page start

--- src/js/script.js
+++ src/js/script.js
@@ -74,4 +74,15 @@ const showSubmenu = (itemsLinks) => {
 }
 
 
+const copyBtn = document.querySelector('.copy-btn');
+const copyBtnTooltip = document.querySelector('.copy-btn__tooltip');
 
+const copyBlogLink = () => {
+  navigator.clipboard.writeText(window.location.href);
+  copyBtnTooltip.classList.add('active');
+  setTimeout(() => {
+    copyBtnTooltip.classList.remove('active');
+  }, 1000);
+}
+
+copyBtn.addEventListener('click', copyBlogLink);
